0db4bcc7fd4468571afaed01c113d7ef9fab72fe
Nurul Mustofa <117705608+Nurulmustofa@users.noreply.github.com>
2023-07-28 13:02:44 +0700

    template

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
-import '../styles/index.css'
+import '../styles/index.css';
+import {apolloProvider} from "@apollo/client/react";
+import { client } from '../lib/apollo';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <apolloProvider client={client}>
         <Component {...pageProps} />
+    </apolloProvider>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Footer from '../components/Footer';
 import PostCard from '../components/PostCard';
 import { getAllPosts } from '../lib/test-data';
+import {client} from '../lib/apollo';
+import {gql} from '@apollo/client';
 
 
 export default function Home({ posts }) {
@@ -14,12 +16,11 @@ export default function Home({ posts }) {
 
       <main>
         <h1 className="title">
-          Headless WordPress Next.js Starter
+          Kelompok KKP Radar Cirebon
         </h1>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        
+        
 
         <div className="grid">
           {
@@ -39,7 +40,21 @@ export default function Home({ posts }) {
 
 export async function getStaticProps(){
 
-  const response = await getAllPosts()
+  const GET_POSTS = gql`
+      query GetAllPosts {
+        posts {
+          nodes {
+            title
+            content
+            uri
+            date
+          }
+        }
+      }
+  `
+  const response = await client.query({
+    query: GET_POSTS
+  });
   const posts = response?.data?.posts?.nodes
   return {
     props: {
